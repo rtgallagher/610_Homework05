49a06a755c7cd67ba99860aa01752799a4d7c85a
Robert Gallagher <rgallag@iu.edu>
2023-10-26 16:59:01 -0400

    second commit, added code

--- llr_functions.R
+++ llr_functions.R
@@ -1,1 +1,27 @@
-#functions
+
+# First level function
+llr = function(x, y, z, omega) {
+    fits = sapply(z, compute_f_hat, x, y, omega)
+    return(fits)
+}
+
+# Second level function
+compute_f_hat = function(z, x, y, omega) {
+    Wz = make_weight_matrix(z, x, omega)
+    X = make_predictor_matrix(x)
+    f_hat = c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
+    return(f_hat)
+}
+
+# Third level function
+make_weight_matrix = function(z, x, omega) {
+    r = (abs(x - z))/(omega)
+    w = ifelse((abs(r) < 1), (1-abs(r)^3)^3, 0)
+    W = return(diag(w))
+    }
+
+
+make_predictor_matrix = function(x) {
+    X <- cbind(1,x)
+    return(X)
+}
--- test_llr.R
+++ test_llr.R
@@ -1,1 +1,38 @@
-#test file
+source("llr_functions.R")
+library(matrixcalc)
+n = 150
+## a very simple regression model
+x = rnorm(n)
+y = rnorm(x + rnorm(n))
+z = seq(-1, 1, length.out = 100)
+library(testthat)
+
+
+test_that("llr output has correct length", {
+    expect_equal(length(llr(x, y, z, omega = 1)), length(z))
+})
+
+
+test_that("make_weight_matrix works on simple cases", {
+    ## check that the output is a diagonal matrix,
+    expect_true(is.diagonal.matrix(make_weight_matrix(z,x, omega=2)))
+    ## check that all the elements are positive,
+    expect_true(all(diag(make_weight_matrix(z,x,omega=2)) >=0))
+    ## check that the weights are correct in simple cases where you know what the output should be
+    expect_equal(round(diag(make_weight_matrix(0.2, c(-0.2,0,0.2), omega = 1)), 3), c(0.820,0.976,1))
+})
+
+test_that("make_predictor_matrix works on simple cases", {
+    ## write tests to check that the dimensions are correct
+    expect_equal(ncol(make_predictor_matrix(rnorm(5))), 2) #  Check # of columns
+    expect_equal(nrow(make_predictor_matrix(rnorm(5))), 5) #  Check # of columns
+    ## the first column is all 1's, etc.
+    expect_equal(make_predictor_matrix(rnorm(4))[,1],rep(1,4)) # Check all 1s in that column
+})
+
+library(reshape2)
+data(french_fries)
+french_fries = french_fries[complete.cases(french_fries),]
+z = seq(0, 15, length.out = 100)
+fits = llr(z = z, x = french_fries$potato, y = french_fries$buttery, omega = 1)
+plot(z, fits)
